e67e5149fdaf8b328c0359db6a6d98801e8ceaed
Hollis <hollisvallotton@reps2.com>
2020-06-13 17:48:34 -0700

    Initial webpack configuration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+	<>
+		<p>Soimple app rendered</p>
+	</>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
